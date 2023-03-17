d03582b58d2749ee51d7a3854216598954d015ee
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-03-17 18:37:22 +0300

    commit tree f040b24bed4d4dcb86a2bd09c18f1101cbd4bd11

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :users, only: [:show] do
+    # nested resource for items
+    resources :items, only: [:index, :show, :create]
+  end
+
+
   resources :items, only: [:index]
-  resources :users, only: [:show]
 end
